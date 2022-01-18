77de00b5de910c4c1c842c11ac83465020e374df
Susan Zea <susanzea16@gmail.com>
2022-01-18 16:47:57 -0500

    preparing to deploy to heroku

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
@@ -6,6 +7,12 @@ const bodyParser = require('body-parser');
 const passport =require('passport');
 const users = require("./routes/api/users")
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 mongoose
   .connect(db, { useNewUrlParser: true })

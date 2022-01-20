fa1693141bfa3176b6b36271c23ca0a10111d2c1
AlexDoes <alexsw.191@gmail.com>
2022-01-20 09:37:48 -0500

    rebase

--- routes/api/files.js
+++ routes/api/files.js
@@ -3,10 +3,15 @@ const router = express.Router();
 const File = require("../../models/File")
 const passport = require("passport");
 <<<<<<< HEAD
+<<<<<<< HEAD
 var ObjectId = require('mongodb').ObjectId;
 const res = require("express/lib/response");
 =======
 >>>>>>> 8f80eda (fixing crud)
+=======
+var ObjectId = require('mongodb').ObjectId;
+const res = require("express/lib/response");
+>>>>>>> a58dc84 (crud all)
 
 router.get('/', (request,response) => {
 
@@ -50,10 +55,14 @@ router.post('/', passport.authenticate('jwt', { session: false }),
 router.patch('/:id', passport.authenticate('jwt', { session: false }), 
     (request,response) => {
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> a58dc84 (crud all)
         File.updateOne({"_id": request.params.id, "user": request.user["id"]}, 
         {$set: {code : request.body.code, name : request.body.name }})    
         .then((res) => response.status(200).json({message: "successfully patched"}))
         .catch(errors => response.status(400).json({message: "something went wrong"}))
+<<<<<<< HEAD
     }
 )
 
@@ -82,15 +91,37 @@ module.exports = router;
     // .catch(errors => response.status(400).json({message: "something went wrong"}))
 =======
         File.updateOne({_id: File.findById(request.params.id)}, {$set: {code : request.body.code, name : request.body.name }})
+=======
+>>>>>>> a58dc84 (crud all)
     }
 )
 
-router.delete('/:id', passport.authenticate('jwt', {session: false})),
+// router.delete('/:id', passport.authenticate('jwt', {session: false}),
+//     (request,response) => {
+//         File.find( {"_id": request.params.id, "user": request.user["id"]}, 
+//         function(error, found) {
+//             if (error) {response.status(400).json({message: "No file found with the user"})} else {
+//                 File.deleteOne({"_id": request.params.id, "user": request.user["id"]})
+//                 .then(() => response.status(200).json({message: "Successfully deleted!"}))
+//                 }
+//             }
+//         )
+//     }
+// )
+router.delete('/:id', passport.authenticate('jwt', {session: false}),
     (request,response) => {
-        File.deleteOne({_id: File.findById(request.params.id)})
-        .then(() => response.status(200).json({message: "successfully deleted"}))
-        .catch(errors => response.status(400).json({message: "something went wrong"}))
+        File.deleteOne( {"_id": request.params.id, "user": request.user["id"]} ) 
+        .then(file => response(file))
+        .catch(errors => response.status(404).json({ NoFilesAttached: 'No files exist'}))
     }
+<<<<<<< HEAD
 
 module.exports = router;
 >>>>>>> 8f80eda (fixing crud)
+=======
+)
+module.exports = router;
+    // File.deleteOne({"_id": request.params.id, "user": request.user["id"]})
+    // .then(() => response.status(200).json({message: "successfully deleted"}))
+    // .catch(errors => response.status(400).json({message: "something went wrong"}))
+>>>>>>> a58dc84 (crud all)

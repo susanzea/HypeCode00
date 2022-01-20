5622f3a9778e67dfd2e7e16dc6dd6ea660322344
Susan Zea <susanzea16@gmail.com>
2022-01-20 17:32:14 -0500

    first level of tag buttons done

--- frontend/src/components/session/login_form.js
+++ frontend/src/components/session/login_form.js
@@ -70,7 +70,7 @@ class LoginForm extends React.Component {
               value={this.state.password}
               onChange={this.handleInput} />
           </div>
-          <button className="submit-button">Log in</button>
+          <button className="submit-button">Login</button>
         </form>
       </div>
     )
